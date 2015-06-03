e5f4b3a4aeb067ec04c4931e164a652ccab22d46
Aulia Agung Maulana <auliaagungmaulana@Maulanas-MacBook-Pro.local>
2015-06-03 12:18:29 +1000

    database

--- db_config.php
+++ db_config.php
@@ -3,8 +3,8 @@
 	*	database connection configuration file to seperate server connection
 	*	values from the rest of the php codes.
 	*/
-	define('DBSERVER', "localhost");
-	define('DBUSER', "infs3202");
-	define('DBPASS', "password");
+	define('DBSERVER', "au-cdbr-azure-east-a.cloudapp.net");
+	define('DBUSER', "bccdef21785421");
+	define('DBPASS', "6271cb31");
 	define('DATABASE', "restaurant");
 ?>
